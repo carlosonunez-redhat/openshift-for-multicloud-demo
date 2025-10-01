ab09146e08af64095ee48f450eba5de8df81abcb
Carlos Nunez <cnunez@redhat.com>
2025-10-01 15:39:07 -0500

    [deploy] add usage
    Signed-off-by: Carlos Nunez <cnunez@redhat.com>

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,32 @@ CONTAINER_BIN="${CONTAINER_BIN:-podman}"
 COMPOSE_FILE="${COMPOSE_FILE:-compose.yaml}"
 COMPOSE_BIN="${COMPOSE_BIN:-podman-compose}"
 
+usage() {
+  cat <<-EOF
+[ENV_VARS] $(basename "$0") [options]
+Deploys the demo.
+
+ENVIRONMENT VARIABLES
+
+  REBUILD       Rebuilds data volumes.
+EOF
+}
+
 _container() {
   "$CONTAINER_BIN" "$@"
+}
+
+_confirm_prereqs_or_fail() {
+  for kvp in "sops;Decrypts config.yaml" \
+    "podman;Runs ansible and other stuff" \
+    "podman-compose;Runs deployment tasks"
+  do
+    bin=$(cut -f1 -d ';' <<< "$kvp")
+    desc=$(cut -f2 -d ';' <<< "$kvp")
+    >/dev/null which "$bin" && continue
+    >&2 echo "ERROR: '$bin' missing ($desc)"
+    return 1
+  done
 }
 
 create_data_volume() {
@@ -28,7 +52,19 @@ deploy() {
     grep --color=always -Ev '(^[a-z0-9]{64}$|openshift-for-multicloud)'
 }
 
+preflight() {
+  _confirm_prereqs_or_fail
+}
+
+show_help_if_requested() {
+  grep -Eq '[-]{1,2}help' <<< "$@" || return 0
+  usage
+  exit 0
+}
+
 set -e
+show_help_if_requested "$@"
+preflight
 create_data_volume
 upload_config_into_data_volume
 deploy
